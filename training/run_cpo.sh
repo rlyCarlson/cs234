cf94e32edb9a1ebdd0114b8e41d43d3222b76d64
Dan Iter <d1@fastino.ai>
2025-03-12 16:54:54 +0000

    cpo

--- training/run_cpo.sh
+++ training/run_cpo.sh
@@ -3,8 +3,8 @@ python dpo.py \
     --train_ds_path ../datasets/dpo_train_subset_data.json \
     --eval_ds_path ../datasets/dpo_train_subset_data.json \
     --model_name 'HuggingFaceTB/SmolLM-360M-Instruct' \
-    --peft_checkpoint ../finetune/models/finetune_baseline_20250308_225249/checkpoint-1872/ \
-    --output_dir ../cpo_checkpoints_dummy \
+    --peft_checkpoint ../finetune_baseline_20250308_225249/checkpoint-1872/ \
+    --output_dir ../cpo_checkpoints_v1 \
     --train_batch_size 4 \
     --eval_batch_size 2 \
     --gradient_accumulation_steps 4 \
